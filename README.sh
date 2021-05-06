c52dca149d56b691ec0f9c100d1fde288435b4b3
philwebsurfer <phil_websurfer@yahoo.com>
2021-05-06 17:11:10 -0500

    README.sh automatización

--- README.sh
+++ README.sh
@@ -8,21 +8,21 @@ done
 git add *.html
 git commit -m 'update of html files'
 
-echo "# Deep Learning Examen Final" > README.md
-echo "" >> README.md
-echo "" >> README.md
+# echo "# Deep Learning Examen Final" > README.md
+# echo "" >> README.md
+# echo "" >> README.md
 
-for file in *.html 
-do
-	file=$(echo $file | sed 's!\.html!!')
-	pdf=""
-	if [[ -f "${file}.pdf" ]] ; then
-		pdf=' [[pdf]](https://philwebsurfer.github.io/dlfinal/'$file'.pdf)'
-	fi
-	echo '* '$file' [[html]](https://philwebsurfer.github.io/dlfinal/'$file'.html)'$pdf >> README.md
-done 
-
-
-git add README.md 
-git commit -m 'update README' 
+# for file in *.html 
+# do
+# 	file=$(echo $file | sed 's!\.html!!')
+# 	pdf=""
+# 	if [[ -f "${file}.pdf" ]] ; then
+# 		pdf=' [[pdf]](https://philwebsurfer.github.io/dlfinal/'$file'.pdf)'
+# 	fi
+# 	echo '* '$file' [[html]](https://philwebsurfer.github.io/dlfinal/'$file'.html)'$pdf >> README.md
+# done 
+# 
+# 
+# git add README.md 
+# git commit -m 'update README' 
 git push
