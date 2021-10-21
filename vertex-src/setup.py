d249c3b21cfa989200e03b1d7e3bf2958681e117
Jorge Altamirano <phil_websurfer@yahoo.com>
2021-10-21 00:59:16 -0500

    updated setup.py trainer/task.py

--- vertex-src/setup.py
+++ vertex-src/setup.py
@@ -1,9 +1,10 @@
 from setuptools import find_packages, setup
 
 REQUIRED_PACKAGES = [
-        "matplotlib>=3.1.2"
-        "tqdm>=4.39.0"
-        "dill>=0.3.2"
+        "matplotlib>=3.1.2",
+        "tqdm>=4.39.0",
+        "dill>=0.3.2",
+        "h5py>=2.10.0"
         ]
 
 setup(
@@ -11,7 +12,7 @@ setup(
     author='Jorge III Altamirano-Astorga',
     author_email='jorge3a(at)gmail.com',
     url='https://philwebsurfer.github.io/dlfinal/vertex-src/',
-    version='0.3',
+    version='0.4',
     install_requires=REQUIRED_PACKAGES,
     packages=find_packages(),
     include_package_data=True,
