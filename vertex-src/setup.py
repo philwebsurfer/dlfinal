2109477a645ea9d349925afb22eba0d6e42fb707
Jorge Altamirano <phil_websurfer@yahoo.com>
2021-12-05 19:16:34 -0600

    updated setup.py trainer/task.py

--- vertex-src/setup.py
+++ vertex-src/setup.py
@@ -12,7 +12,7 @@ setup(
     author='Jorge III Altamirano-Astorga',
     author_email='jorge3a(at)gmail.com',
     url='https://philwebsurfer.github.io/dlfinal/vertex-src/',
-    version='0.21',
+    version='0.22',
     install_requires=REQUIRED_PACKAGES,
     packages=find_packages(),
     include_package_data=True,
